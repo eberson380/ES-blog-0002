aa48ab545150f759c4ddeafdc72820a5ce6fd72e
Eberson Silva <ebersonsilva380@gmail.com>
2025-03-25 07:53:02 -0300

    banner magalu pronto

--- script.js
+++
@@ -1,9 +0,0 @@
-let index = 0
-const slides = document.querySelector(".slides")
-
-function nextSlide() {
-  index = (index + 1) % 4 // 4 é o número de imagens
-  slides.style.transform = `translateX(${-index * 100}%)`
-}
-
-setInterval(nextSlide, 2000) // Troca a cada 2 segundos
